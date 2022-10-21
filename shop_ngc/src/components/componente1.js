57ec50b521b651d32c596c76984211c3142cb10f
Francisco <franciscoperezvillalba@gmail.com>
2022-10-20 20:57:59 -0500

    se aplica NavLink, Routes y styled components, se crea una vista y dos botones

---
+++ shop_ngc/src/components/componente1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Componente1 = () => {
+    return ( 
+        <div>
+            <h1>Lista de productos</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, facilis non dolore magni sunt laboriosam ex commodi nostrum eos hic ipsum, minima eum explicabo beatae ullam repellendus necessitatibus deleniti neque?</p>
+        </div>
+     );
+}
+ 
+export default Componente1;
---
+++ shop_ngc/src/components/componente2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Componente2 = () => {
+    return ( 
+        <div>
+            <h1>Carrito</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, expedita. Ad nulla illo aut, deleniti eius tenetur, laborum accusamus culpa totam nihil eveniet velit! Sit, nisi? Corrupti non eaque at.</p>
+        </div>
+     );
+}
+ 
+export default Componente2;
