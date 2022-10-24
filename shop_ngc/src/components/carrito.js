5e9ce6f651da4cdd3f2789a4f7ebf5580354eef9
Francisco <franciscoperezvillalba@gmail.com>
2022-10-24 13:25:06 -0500

    se crea diseño de carrito de compras

--- shop_ngc/src/components/carrito.js
+++ shop_ngc/src/components/carrito.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import datos from './productos.json'
  const Carrito = () => {
+    let carrito = [];
+    
+    for(let i=0; i<5;i++){
+        carrito.push(datos[i])
+    }
+    
     return ( 
-        <>
-            <h1>Este es un carrito de compras</h1>
-        </>
+        <div>
+            <h2>Carrito de compras</h2>
+            <table className='tablaCarrito'>
+                <tbody>
+                    <tr>
+                        <th>Imagen</th>
+                        <th>Cantidad</th>
+                        <th>Producto</th>
+                        <th>Valor</th>
+                        <th>Total</th>
+                    </tr>
+                    <tr>
+                        <td><img src='https://cdn-icons-png.flaticon.com/512/1625/1625099.png' alt='Imagen producto' width={45}/></td>
+                        <td>2</td>
+                        <td>Fruta</td>
+                        <td>$50</td>
+                        <td>$100</td>
+                    </tr>
+                    <tr>
+                        <td><img src='https://cdn-icons-png.flaticon.com/512/1625/1625099.png' alt='Imagen producto' width={45}/></td>
+                        <td>1</td>
+                        <td>Vegetal</td>
+                        <td>$50</td>
+                        <td>$50</td>
+                    </tr>
+                </tbody>
+
+
+                {/* <h3>Carrito de compras</h3>
+                {carrito.length > 0 ?
+            carrito.map((producto, index)=>{
+                return(
+                    <div className='Producto' key={index}>
+                        <p>{producto.name}</p>
+                    </div>
+                )
+            })
+            :
+            <p>Carrito Vacío</p>    
+            } */}
+            </table>
+            <footer className='totalCarrito'>
+                <p>Total: $</p>
+
+                <button className='botonCarrito'>Finalizar compra</button>
+                <button className='botonCarrito'>Cancelar</button>
+            </footer>
+        </div>
      );
  }
   
