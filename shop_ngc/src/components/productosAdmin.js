69b33add53cf2f83fdd585e6a7aa6a19155707ac
RAUL PERILLA SANTAMARIA <raulps1971@gmail.com>
2022-10-27 20:28:05 -0500

    1 comit codigo
    actualice algo de estilos en fuentes y creee lista de productos

--- shop_ngc/src/components/productosAdmin.js
+++ shop_ngc/src/components/productosAdmin.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import producto from '../database/imports/Frutas_y_verduras.json'
 
 const ListaAdmin = () => {
     return ( 
-        <>
+        <div>
+            
             <h1>ListaAdmin producto</h1>
-        </>
+
+            <table className='tablaProducto'>
+                <tbody>
+                    <tr>
+                        <th>Codigo del Producto</th>
+                        <th>Imagen</th>
+                        <th>Unidad Medida</th>
+                        <th>Producto</th>
+                        <th>Valor</th>
+                    </tr>
+                    {
+                        producto.map((producto, index)=>{
+                            return(
+                                <tr key={index}>
+                                    <td>{producto.up}</td>
+                                    <td><img src={producto.imagen} alt={producto.name} width={15}/></td>
+                                    <td>{producto.c} </td>
+                                    <td>{producto.name}</td>
+                                    <td>${producto.d}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+
+            </table>
+        </div>
      );
-}
- 
-export default ListaAdmin;
+ }
+  
+ export default ListaAdmin;
