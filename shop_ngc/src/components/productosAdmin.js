87e3445e6c78b6b7c452fb05a5630f50a727ef6f
Francisco <franciscoperezvillalba@gmail.com>
2022-11-02 15:03:31 -0500

    Se cambia diseño lista productos

--- shop_ngc/src/components/productosAdmin.js
+++ shop_ngc/src/components/productosAdmin.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import data from './jsonProductos.json';
-import '../styles/productosAdmin.css';
+import '../styles/styleProductosList.css';
 
 
 const ListaAdmin = () => {
     return ( 
-        <div className = 'productos'>
+        <div className = 'ContenedorProductos'>
+            
             {data.map((produc, index)=> {
                 return(
-                    <div key ={index}>
+                    <div key ={index} className='Producto'>
                          <img className='img' src={produc.imagen} alt={produc.descripcion}  />
                         <p className= 'product_nombre'>{produc.nombre}</p>
                         <p className ='product_valor'>${produc.valor}</p>
-                        <p className ='prodct_stock'>stock{produc.stock}</p>
-
+                        <p className ='prodct_stock'>Stock: {produc.stock}</p>
                     </div>
                 )
             }  
--- shop_ngc/src/components/productosCliente.js
+++ shop_ngc/src/components/productosCliente.js
@@ -1,90 +1,26 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import data from './jsonProductos.json';
+import '../styles/styleProductosList.css';
 
 const ListaCliente = () => {
     return(
-        <Fragment>
-            <h1  id = "encabezado_productos"> Productos Disponibles </h1>
-            <section id="productos" className='container mt-3'>
-                <div className='row'>
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src="./imagenes/fresas.png" alt="Fresas"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'> Fresas </a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating_inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 5 Reviews </span>
-                                </div>
-                                <p className='card-text'> $6.500 x Kg </p>
-                                    
-                                
-                            </div>
-                        </div>
+        <div className = 'ContenedorProductos'>
+            
+            {data.map((produc, index)=> {
+                return(
+                    <div key ={index} className='Producto'>
+                         <img className='img' src={produc.imagen} alt={produc.descripcion}  />
+                        <p className= 'product_nombre'>{produc.nombre}</p>
+                        <p className ='product_valor'>${produc.valor}</p>
+                        <p className ='prodct_stock'>Stock: {produc.stock}</p>
+                        <button className='btnAgregarProducto'>Agregar al carrito</button>
                     </div>
-                    </div>
-            </section>
-            <section id="productos" className='container mt-3'>
-                <div className='row'>
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src="./imagenes/berenjena.png" alt="Berenjena"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'> Berenjena </a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 5 Reviews </span>
-                                </div>
-                                <p className='card-text'> $15.500 x Kg </p>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-            </section>
-            <section id="productos" className='container mt-3'>
-                <div className='row'>
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src="./imagenes/cebolla.png" alt="Cebolla Blanca"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'> Cebolla Blanca </a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 5 Reviews </span>
-                                </div>
-                                <p className='card-text'> $2.500 x Kg </p>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-            </section>
-            <section id="productos" className='container mt-3'>
-                <div className='row'>
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src="./imagenes/piña.png" alt="Piña Oro Miel"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'> Piña Oro Miel </a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 5 Reviews </span>
-                                </div>
-                                <p className='card-text'> $4.000 x Kg </p> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+                )
+            }  
+            
+            )}      
+        </div>
 
-        </Fragment>
-       
         
     );
 }
